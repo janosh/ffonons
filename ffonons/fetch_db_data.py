3eb2a80ad2f310f1d31386550aa871398e167c63
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-12-07 19:11:23 -0800

    add get_mp_ph_data() in ffonons/fetch_db_data.py

---
+++ ffonons/fetch_db_data.py
@@ -0,0 +1,55 @@
+import gzip
+import json
+import os
+from typing import TYPE_CHECKING
+
+from mp_api.client import MPRester
+from pymatviz.io import save_fig
+
+from ffonons import DATA_DIR, FIGS_DIR, bs_key, dos_key
+from ffonons.plots import plot_phonon_bs, plot_phonon_dos
+
+if TYPE_CHECKING:
+    from emmet.core.phonon import PhononBSDOSDoc
+    from pymatgen.core import Structure
+
+__author__ = "Janosh Riebesell"
+__date__ = "2023-12-07"
+
+mpr = MPRester(mute_progress_bars=True)
+
+
+def get_mp_ph_data(mp_id: str, docs_dir: str = DATA_DIR) -> None:
+    """Get phonon data from MP and save it to disk. Also plot and save phonon band
+    structure and DOS. Returns the structure and the phonon data as dict.
+    """
+    struct: Structure = mpr.get_structure_by_material_id(mp_id)
+    struct.properties["id"] = mp_id
+
+    id_formula = f"{mp_id}-{struct.formula.replace(' ', '')}"
+    figs_out_dir = f"{FIGS_DIR}/{id_formula}"
+    mp_dos_fig_path = f"{figs_out_dir}/dos-mp.pdf"
+    mp_bands_fig_path = f"{figs_out_dir}/bands-mp.pdf"
+    # struct.to(filename=f"{out_dir}/struct.cif")
+
+    if not os.path.isfile(mp_dos_fig_path) or not os.path.isfile(mp_bands_fig_path):
+        mp_phonon_doc: PhononBSDOSDoc = mpr.materials.phonon.get_data_by_id(mp_id)
+
+        mp_doc_dict = {
+            dos_key: mp_phonon_doc.ph_dos.as_dict(),
+            bs_key: mp_phonon_doc.ph_bs.as_dict(),
+        }
+        # create dir only if MP has phonon data
+        os.makedirs(figs_out_dir, exist_ok=True)
+        with gzip.open(f"{docs_dir}/{id_formula}-mp.json.gz", "wt") as file:
+            file.write(json.dumps(mp_doc_dict))
+
+        ax_bs_mp = plot_phonon_bs(mp_phonon_doc.ph_bs, "MP", struct)
+        # always save figure right away, plotting another figure will clear the axis!
+        save_fig(ax_bs_mp, mp_bands_fig_path)
+
+        ax_dos_mp = plot_phonon_dos({"MP": mp_phonon_doc.ph_dos}, "MP", struct)
+
+        save_fig(ax_dos_mp, mp_dos_fig_path)
+
+    return struct, mp_doc_dict, figs_out_dir
--- ffonons/run_phonon_maker.py
+++ ffonons/run_phonon_maker.py
@@ -6,22 +6,13 @@ import shutil
 from time import perf_counter
 
 import atomate2.forcefields.jobs as ff_jobs
-from atomate2.common.schemas.phonons import PhononBSDOSDoc as AtomPhononBSDOSDoc
+from atomate2.common.schemas.phonons import PhononBSDOSDoc as Atomate2PhononBSDOSDoc
 from atomate2.forcefields.flows.phonons import PhononMaker
-from emmet.core.phonon import PhononBSDOSDoc
 from jobflow import run_locally
 from mp_api.client import MPRester
-from pymatgen.core import Structure
 from pymatviz.io import save_fig
 
-from ffonons import (
-    DATA_DIR,
-    FIGS_DIR,
-    ROOT,
-    bs_key,
-    dos_key,
-    find_last_dos_peak,
-)
+from ffonons import DATA_DIR, ROOT, bs_key, dos_key, fetch_db_data, find_last_dos_peak
 from ffonons.plots import plot_phonon_bs, plot_phonon_dos
 
 __author__ = "Janosh Riebesell"
@@ -34,7 +25,6 @@ docs_dir = f"{DATA_DIR}/phonon-bs-dos"
 shutil.rmtree(runs_dir, ignore_errors=True)  # remove old runs
 os.makedirs(runs_dir, exist_ok=True)
 
-# common_relax_kwds = dict(fmax=0.00001, relax_cell=True)
 common_relax_kwds = dict(fmax=0.00001)
 mace_kwds = dict(
     model="https://tinyurl.com/y7uhwpje",
@@ -66,60 +56,18 @@ models = {
 }
 
 
-# see https://legacy.materialsproject.org/#search/materials/{%22has_phonons%22%3Atrue}
-# for all mp-ids with phonons data
-mp_ids = (
-    "mp-504729 mp-3486 mp-23137 mp-8799 mp-28591 mp-989639 mp-10182 mp-672 "
-    "mp-5342 mp-27422 mp-29750 mp-9919 mp-2894 mp-406 mp-567776 mp-8192 mp-697144 "
-    "mp-15794 mp-23268 mp-3163 mp-14069 mp-23405 mp-3056 mp-27207 mp-11175 mp-11718 "
-    "mp-9566 mp-14092 mp-1960 mp-30459 mp-772290 mp-996943 mp-571093 mp-149 mp-504729 "
-    "mp-3486 mp-23137 mp-8799 mp-28591 mp-989639 mp-10182 mp-672 mp-5342 mp-27422 "
-    "mp-29750 mp-9919 mp-2894 mp-406 mp-567776 mp-8192 mp-697144 mp-15794 mp-23268 "
-    "mp-3163 mp-14069 mp-23405 mp-3056 mp-27207 mp-11175 mp-11718 mp-9566 mp-14092 "
-    "mp-1960 mp-30459 mp-772290 mp-996943 mp-571093 mp-149 mp-10418 mp-8235 mp-3666 "
-    "mp-1020059 mp-27253 mp-23485 mp-20463 mp-7173 mp-14039 mp-7774 mp-961713 "
-    "mp-20072 mp-19795 mp-27385 mp-10163 mp-36508 mp-8161 mp-7684 mp-27193 mp-866291 "
-    "mp-14437 mp-10616 mp-867335 mp-7773 mp-28264 mp-638729 mp-9567 mp-22925 "
-    "mp-989531 mp-549697 mp-15988 mp-7639 mp-8446 mp-13541 mp-561947 mp-553025 mp-4236 "
-    "mp-989526 mp-9200 mp-755013 mp-30056 mp-862871 mp-752676 mp-553374 mp-3359 "
-    "mp-2530 mp-3637 mp-29149 mp-13798 mp-2074 mp-11583 mp-8188 mp-9881 mp-753802 "
-    "mp-8979 mp-643257 mp-10547 mp-551243 mp-8217 mp-6870 mp-989567 mp-4187 mp-1747 "
-    "mp-23023 mp-996962 mp-8932".split()
-)
+with open(f"{DATA_DIR}/mp-ids-with-phonons.yml") as file:
+    mp_ids = file.read().splitlines()[2:]
+bad_ids = [mp_id for mp_id in mp_ids if not mp_id.startswith("mp-")]
+assert len(bad_ids) == 0, f"{bad_ids=}"
 
 
 # %%
 mpr = MPRester(mute_progress_bars=True)
 
 for mp_id in mp_ids:
-    struct: Structure = mpr.get_structure_by_material_id(mp_id)
-    struct.properties["id"] = mp_id
-
+    struct, mp_doc_dict, figs_out_dir = fetch_db_data(mp_id, docs_dir)
     id_formula = f"{mp_id}-{struct.formula.replace(' ', '')}"
-    figs_out_dir = f"{FIGS_DIR}/{id_formula}"
-    mp_dos_fig_path = f"{figs_out_dir}/dos-mp.pdf"
-    mp_bands_fig_path = f"{figs_out_dir}/bands-mp.pdf"
-    # struct.to(filename=f"{out_dir}/struct.cif")
-
-    if not os.path.isfile(mp_dos_fig_path) or not os.path.isfile(mp_bands_fig_path):
-        mp_phonon_doc: PhononBSDOSDoc = mpr.materials.phonon.get_data_by_id(mp_id)
-
-        mp_doc_dict = {
-            dos_key: mp_phonon_doc.ph_dos.as_dict(),
-            bs_key: mp_phonon_doc.ph_bs.as_dict(),
-        }
-        # create dir only if MP has phonon data
-        os.makedirs(figs_out_dir, exist_ok=True)
-        with gzip.open(f"{docs_dir}/{id_formula}-mp.json.gz", "wt") as file:
-            file.write(json.dumps(mp_doc_dict))
-
-        ax_bs_mp = plot_phonon_bs(mp_phonon_doc.ph_bs, "MP", struct)
-        # always save figure right away, plotting another figure will clear the axis!
-        save_fig(ax_bs_mp, mp_bands_fig_path)
-
-        ax_dos_mp = plot_phonon_dos({"MP": mp_phonon_doc.ph_dos}, "MP", struct)
-
-        save_fig(ax_dos_mp, mp_dos_fig_path)
 
     for model, makers in models.items():
         key = model.lower().replace(" ", "-")
@@ -146,7 +94,7 @@ for mp_id in mp_ids:
             # phonon_flow.draw_graph().show()
 
             os.makedirs(root_dir := f"{runs_dir}/{key}", exist_ok=True)
-            responses = run_locally(
+            result = run_locally(
                 phonon_flow,
                 root_dir=root_dir,
                 log=False,
@@ -154,11 +102,8 @@ for mp_id in mp_ids:
             )
             print(f"{model} took: {perf_counter() - start:.2f} s")
 
-            ml_phonon_doc: AtomPhononBSDOSDoc = next(
-                val
-                for val in responses.values()
-                if isinstance(val[1].output, AtomPhononBSDOSDoc)
-            )[1].output
+            ml_phonon_doc: Atomate2PhononBSDOSDoc = result[phonon_flow[-1].uuid].output
+
             ml_doc_dict = {
                 dos_key: ml_phonon_doc.phonon_dos.as_dict(),
                 bs_key: ml_phonon_doc.phonon_bandstructure.as_dict(),
