6862b36afcb16dd3160aebbb84f6ea809b199632
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-12-18 12:02:55 -0800

    add sveltekit site to show interactive plotly DOS and BS figs

---
+++ site/src/routes/+layout.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  import { goto } from '$app/navigation'
+  import { base } from '$app/paths'
+  import { page } from '$app/stores'
+  import { repository } from '$site/package.json'
+  import { CmdPalette } from 'svelte-multiselect'
+  import Toc from 'svelte-toc'
+  import { CopyButton, GitHubCorner } from 'svelte-zoo'
+  import '../app.css'
+
+  $: headingSelector = `main :is(${
+    $page.url.pathname === `${base}/api` ? `h1, h2, h3, h4` : `h2`
+  }):not(.toc-exclude)`
+
+  const file_routes = Object.keys(import.meta.glob(`./**/+page.{svx,svelte,md}`))
+    .filter((key) => !key.includes(`/[`))
+    .map((filename) => {
+      const parts = filename.split(`/`)
+      return `/` + parts.slice(1, -1).join(`/`)
+    })
+
+  const actions = file_routes.map((name) => {
+    return { label: name, action: () => goto(`${base}${name.toLowerCase()}`) }
+  })
+</script>
+
+<CmdPalette {actions} placeholder="Go to..." />
+
+<CopyButton global />
+
+<Toc {headingSelector} breakpoint={1250} warnOnEmpty={false} />
+
+{#if ![base, `/`].includes($page.url.pathname)}
+  <a href="{base}/" aria-label="Back to index page">&laquo; home</a>
+{/if}
+
+<GitHubCorner href={repository} />
+
+<slot />
+
+<footer>
+  <a href="{repository}/issues" rel="external">Issues</a>
+  <a href="{repository}/discussions" rel="external">Discussions</a>
+</footer>
+
+<style>
+  footer {
+    background: #00061a;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 3ex;
+    place-content: center;
+    place-items: center;
+    margin: 2em 0 0;
+    padding: 3vh 3vw;
+  }
+  a[aria-label] {
+    font-size: 15pt;
+    position: absolute;
+    top: 2em;
+    left: 2em;
+    background-color: rgba(255, 255, 255, 0.1);
+    padding: 1pt 5pt;
+    border-radius: 3pt;
+    transition: 0.2s;
+  }
+  a[aria-label]:hover {
+    background-color: rgba(255, 255, 255, 0.2);
+  }
+</style>
---
+++ site/src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { browser } from '$app/environment'
+  import Readme from '$root/README.md'
+
+  const ph_dos_figs = import.meta.glob(`$figs/mace-mp0-phonondb/*-dos-*.svelte`, {
+    eager: true,
+    import: `default`,
+  })
+</script>
+
+<main>
+  <Readme />
+  {#if browser}
+    {#each Object.values(ph_dos_figs) as ph_dos_fig}
+      <svelte:component this={ph_dos_fig} />
+    {/each}
+  {/if}
+</main>
+
+<style>
+  :global(h1[align='center']) {
+    display: flex;
+    font-size: clamp(2rem, 2rem + 2vw, 3rem);
+    place-items: center;
+    place-content: center;
+    gap: 6pt;
+  }
+  :global(a:has(img[alt='Docs'])) {
+    display: none;
+  }
+</style>
