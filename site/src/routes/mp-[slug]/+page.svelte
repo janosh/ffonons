6d37b8f47c621c0490f0cae9ad92f2fa8d6630ec
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-12-18 12:33:32 -0800

    add /mp-[slug] page for showing individual DOS and BS (in future) plots

---
+++ site/src/routes/mp-[slug]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { browser } from '$app/environment'
+  // import { page } from '$app/stores'
+  export let data
+
+    // $: mp_id = input_value.trim().toLowerCase()
+    // $: href = `https://legacy.materialsproject.org/materials/${mp_id}`
+</script>
+
+<main>
+  <!-- <h1><a {href}>MP-{$page.params.slug}</a></h1> -->
+
+  {#if browser}
+    <svelte:component this={data.DosFig} style="margin: 4em 0 0;" />
+  {/if}
+</main>
